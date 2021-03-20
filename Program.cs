9f4f74d2e4568e6e849a2b608224623e749e778e
Ana-Costa12 <claudiasantborges@gmail.com>
2021-03-20 20:30:25 -0300

    COnversão de graus

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula_de_PC1__F_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fahrenheit;
+            double Celsius = 32;
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Você sabia que o °C não é a ùnica medida de temperatura?");
+            Console.WriteLine("========================================================");
+            Console.ResetColor();
+            Console.WriteLine("\nExiste o grau Fahrenheit.\n\nAgora digite um valor para a temperatura em Fahrenheit:");
+            Fahrenheit = double.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.WriteLine($"{Fahrenheit}°F equivale á exatamente: {Fahrenheit - Celsius / 1.8 }°C");
+            Console.ResetColor();
+            Console.WriteLine("Aperte qualquer botão para sair...");
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+    }
+}
